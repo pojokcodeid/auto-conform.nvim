d597dd1277a3c740efba9147df6fa46e0f0eb927
asep.komarudin <asep.komarudin@dataon.com>
2024-07-02 20:44:59 +0700

    fix: error opts

--- lua/auto-conform/init.lua
+++ lua/auto-conform/init.lua
@@ -1,13 +1,17 @@
 local M = {}
 
 M.setup = function(opts)
-	opts.formatters = opts.formatters or {}
-	opts.formatters_by_ft = opts.formatters_by_ft or {}
-	opts.ensure_installed = opts.ensure_installed or {}
-	opts.lang_maps = opts.lang_maps or {}
-	opts.name_maps = opts.name_maps or {}
-	opts.add_new = opts.name_maps or {}
-	opts.ignore = opts.ignore or {}
+  if opts then
+	  opts.formatters = opts.formatters or {}
+	  opts.formatters_by_ft = opts.formatters_by_ft or {}
+	  opts.ensure_installed = opts.ensure_installed or {}
+	  opts.lang_maps = opts.lang_maps or {}
+	  opts.name_maps = opts.name_maps or {}
+	  opts.add_new = opts.name_maps or {}
+	  opts.ignore = opts.ignore or {}
+	else
+	  opts = {}
+  end
 	require("auto-conform.masonregister").setup(opts)
 end
 
