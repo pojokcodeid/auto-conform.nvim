08f9b7b43c847159413d64e5a793fe0b6ccb7d62
asep.komarudin <asep.komarudin@dataon.com>
2024-07-03 20:42:52 +0700

    enc: move string to table

--- lua/auto-conform/utils.lua
+++ lua/auto-conform/utils.lua
@@ -1,33 +1,33 @@
 local M = {}
 M.keymaps = {
-	["c++"] = "cpp",
-	["c#"] = "cs",
-	["jsx"] = "javascriptreact",
+  ["c++"] = "cpp",
+  ["c#"] = "cs",
+  ["jsx"] = "javascriptreact",
 }
 
 M.name_maps = {
-	["cmakelang"] = "cmake_format",
-	["deno"] = "deno_fmt",
-	["elm-format"] = "elm_format",
-	["gdtoolkit"] = "gdformat",
-	["nixpkgs-fmt"] = "nixpkgs_fmt",
-	["opa"] = "opa_fmt",
-	["php-cs-fixer"] = "php_cs_fixer",
-	["ruff"] = "ruff_format",
-	["sql-formatter"] = "sql_formatter",
-	["xmlformatter"] = "xmlformat",
+  ["cmakelang"] = "cmake_format",
+  ["deno"] = "deno_fmt",
+  ["elm-format"] = "elm_format",
+  ["gdtoolkit"] = "gdformat",
+  ["nixpkgs-fmt"] = "nixpkgs_fmt",
+  ["opa"] = "opa_fmt",
+  ["php-cs-fixer"] = "php_cs_fixer",
+  ["ruff"] = "ruff_format",
+  ["sql-formatter"] = "sql_formatter",
+  ["xmlformatter"] = "xmlformat",
 }
 
 M.add_new = {
-	jsonc = {"prettier"},
-	json = {"prettier"},
-	typescriptreact = {"prettier"},
+  ["jsonc"] = { "prettier" },
+  ["json"] = { "prettier" },
+  ["typescriptreact"] = { "prettier" },
 }
 
 M.ignore = {
-	["php"] = {"tlint"},
-	["lua"] = {"ast-grep"},
-	["kotlin"] = {"ktlint"},
+  ["php"] = { "tlint" },
+  ["lua"] = { "ast-grep" },
+  ["kotlin"] = { "ktlint" },
 }
 
 return M
